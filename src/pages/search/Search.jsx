8f460bd60f93ed58baae94ffb489b8eb97912dd1
Wesley <wesley_henrique15@hotmail.com>
2023-04-07 22:58:49 -0300

    ADD person search

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -6,11 +6,9 @@ import { Col, Row } from "react-bootstrap";
 import api from './../../services/api';
 import CardMovie from "../../components/CardMovie.jsx";
 import Loading from "../../components/Loading.jsx";
+import CardPerson from "../../components/CardPerson.jsx";
 
 import "./Search.css"
-
-
-
 
 export default function Search(props) {
     const { value } = useParams();
@@ -18,15 +16,15 @@ export default function Search(props) {
     const [page, setPage] = useState(1);
     const [total, setTotal] = useState(1);
     const [loading, setLoading] = useState(true);
+    const [isPerson, setIsPerson] = useState(false);
 
     useEffect(() => {
         setLoading(true);
         setPage(1);
-    }, [value])
+    }, [value, isPerson])
 
 
     useEffect(() => {
-
         async function loadStreaming() {
             const responseSearch = await api.get(`/search/multi`, {
                 params: {
@@ -75,12 +73,32 @@ export default function Search(props) {
     }
     return (
         <div className="Search">
-            <h2>Procurando: <span id="value">{value}</span></h2>
+            <div className="d-flex">
+                <h2 className="me-4">Pesquise por:</h2>
+                <h2>
+                    <span onClick={() => setIsPerson(false)} className={`me-2 ${isPerson ? "text-muted" : "active"}`} >Filmes e Series</span>
+                    <>ou</>
+                    <span onClick={() => setIsPerson(true)} className={`ms-2 ${isPerson ? "active" : "text-muted"}`} >Pessoas</span>
+                </h2>
+            </div>
+            <h4 className="pt-4 p-2">Resultado: <i className="text-muted">{value}</i></h4>
             <Row className="listSearch">
                 {streaming && (streaming.results.map((item, i) => {
-                    if (item.media_type === "person") {
-                        return (null);
-                    } if (!item.poster_path) {
+                    if (isPerson) {
+                        if (item.media_type === "person") {
+                            if (item.profile_path) {
+                                return (
+                                    <Col xs={12} sm={6} md={4} lg={3} xl={3} xxl={2} key={`${item.id}_[${i}]`} className="p-2">
+                                        <CardPerson name={item.name} id={item.id} profile_path={item.profile_path}
+                                            character={item.character} />
+                                    </Col>
+                                )
+                            }
+                        } else {
+                            return null;
+                        }
+                    }
+                    if (!item.poster_path) {
                         return (null);
                     }
                     return (
@@ -88,6 +106,8 @@ export default function Search(props) {
                             <CardMovie type={item.media_type} id={item.id} title={item.title || item.name} poster={item.poster_path} average={item.vote_average} date={item.release_date || item.first_air_date} />
                         </Col>
                     )
+
+
                 }))}
                 <Col xs={12} sm={6} md={4} lg={3} xl={3} xxl={2}>
                     <li id="sentry" className="h-100 d-flex flex-column justify-content-center">
